46e2cc7d3d7a9bad263fa1422bff6ee2d10ba184
Nicholas Emmons <emmonsnicholas7@gmail.com>
2023-06-14 13:45:53 -0400

    Added Swordsmith Model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+JOBS = (
+    ('P','Polish')
+    ('S','Sharpen')
+    ('R','Remodel')
+)
+
 # Create your models here.
 class Sword(models.Model):
     name = models.CharField(max_length=100)
@@ -11,3 +17,14 @@ class Sword(models.Model):
         return self.name
     def get_absolute_url(self):
         return reverse('detail', kwargs={'sword_id': self.id})
+
+class Swordsmith(models.Model):
+    date = models.DateField()
+    job = models.CharField(
+        max_length=1,
+        choices=JOBS,
+        default=JOBS[0][0]
+        )
+    sword = models.ForeignKey(Sword, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.get_job_display()} on {self.date}"
